ed0c7a23d8f08013b776cd0719fb3738f3819299
Rougan <cristian.pineros@hotmail.com>
2024-10-04 17:18:12 -0500

    finished C5 homework

--- C5 Fundamentos Javascript/ejercicios/14.js
+++ C5 Fundamentos Javascript/ejercicios/14.js
@@ -2,10 +2,15 @@ function mayorQueCincuenta(num) {
   // La función recibe un argumento llamado num.
   // Retorna true si el argumento "num" es mayor que cincuenta.
   // De lo contrario, retorna false.
-  // Por ejemplo: 
+  // Por ejemplo:
   // 51 ---> true
   // 15 ---> false
   // Tu código:
+  if (num > 50) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = mayorQueCincuenta;
